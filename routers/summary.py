e9bf630312bd21e5fcf6553f12f8f8d87f1a9111
amogh-agrawal-couture <amogh.agrawal@couture.ai>
2026-01-13 13:11:45 +0530

    multi dependency

--- routers/summary.py
+++ routers/summary.py
@@ -4,21 +4,20 @@ from sqlalchemy.orm import Session
 from fastapi.responses import FileResponse
 import pandas as pd
 import os
-
+from starlette import status
 from database import get_db
 from models import Product
 from analysis import generate_summary
 from auth import get_current_user
 from schemas import SummaryRow
 
-router = APIRouter(prefix="/summary", tags=["Summary"])
+router = APIRouter(prefix="/summary", tags=["Summary"], dependencies=[Depends(get_current_user)])
 
 CSV_PATH = "summary.csv"
 
 
 @router.get("/", response_model=list[SummaryRow])
 def get_summary(
-    current_user: str = Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
     df = pd.read_sql(db.query(Product).statement, db.bind)
@@ -33,7 +32,7 @@ def get_summary(
     }
     if not expected_cols.issubset(summary_df.columns):
         raise HTTPException(
-            status_code=500,
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Summary dataframe columns mismatch",
         )
 
@@ -44,7 +43,6 @@ def get_summary(
 
 @router.get("/download")
 def download_summary(
-    current_user: str = Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
     if not os.path.exists(CSV_PATH):
